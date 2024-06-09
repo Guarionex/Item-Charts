0f44c55cd274e2db4d7ca3c015828a53e2502be8
Edwin Alt Karaya <chespirana@gmail.com>
2024-06-09 04:13:50 +0000

    Editting CORS

--- backend/src/main/kotlin/com/karaya/itemapi/config/CorsConfig.kt
+++ backend/src/main/kotlin/com/karaya/itemapi/config/CorsConfig.kt
@@ -22,25 +22,8 @@ class CorsConfig {
                     )
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
-                    .exposedHeaders("Location")
                     .allowCredentials(true)
             }
         }
-    }
-
-    @Bean
-    fun corsFilter(): CorsFilter {
-        val source = UrlBasedCorsConfigurationSource()
-        val config = CorsConfiguration()
-        config.allowCredentials = true
-        config.allowedOrigins = listOf(
-            "http://localhost:3000",
-            "http://a7f06cbfd91954b0cbee1be17310c5c6-552271409.us-west-2.elb.amazonaws.com"
-        )
-        config.allowedHeaders = listOf("*")
-        config.exposedHeaders = listOf("Location")
-        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        source.registerCorsConfiguration("/**", config)
-        return CorsFilter(source)
     }
 }
