8a4e1322be689e909f746d0c6212888cb10adaa9
Edwin Alt Karaya <chespirana@gmail.com>
2024-06-09 04:14:56 +0000

    Wrong package

--- backend/src/main/kotlin/com/karaya/itemapi/config/CorsConfig.kt
+++ backend/src/main/kotlin/com/karaya/itemapi/config/CorsConfig.kt
@@ -1,4 +1,4 @@
-package com.karaya.tetrahedron.config
+package com.karaya.itemapi.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
